5b0f2836fb7bca58fc9d261ecb56c3c09f5cee54
HuyNguyen1405-Dev <vanhuy141520@gmail.com>
2024-01-08 16:19:49 +0700

    Update code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/indexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<IndexPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
